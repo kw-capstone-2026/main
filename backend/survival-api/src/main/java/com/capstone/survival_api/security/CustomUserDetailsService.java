53d40dd2c5e443f25ae8af5364187f43754ffa93
백지윤 <jy.baek079@gmail.com>
2026-05-16 14:58:58 +0900

    feat: Security 레이어 생성

---
+++ backend/survival-api/src/main/java/com/capstone/survival_api/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.capstone.survival_api.security;
+
+import com.capstone.survival_api.domain.entity.User;
+import com.capstone.survival_api.domain.repository.UserRepository;
+import com.capstone.survival_api.exception.AppException;
+import com.capstone.survival_api.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
